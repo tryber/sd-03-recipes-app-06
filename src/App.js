12d0cfce2d08351e0d3cffc0f016d7f461a40fc1
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-26 18:11:24 -0300

    codeclimate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import ProviderSearch from './context/ProviderSearch';
 import Comidas from './pages/Comidas';
@@ -19,20 +18,20 @@ import './App.css';
 function App() {
   return (
     <ProviderSearch>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/bebidas" component={Bebidas} />
-          <Route path="/comidas" component={Comidas} />
-          <Route path="/explorar/comidas/ingredientes" component={ExplorarComidasIngredientes} />
-          <Route path="/explorar/bebidas/ingredientes" component={ExplorarBebidasIngredientes} />
-          <Route path="/explorar/comidas/area" component={ExplorarComidasArea} />
-          <Route path="/explorar/comidas" component={ExplorarComidas} />
-          <Route path="/explorar/bebidas" component={ExplorarBebidas} />
-          <Route path="/explorar" component={Explorar} />
-          <Route path="/perfil" component={Perfil} />
-          <Route path="/receitas-feitas" component={ReceitasFeitas} />
-          <Route path="/receitas-favoritas" component={ReceitasFavoritas} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/bebidas" component={Bebidas} />
+        <Route path="/comidas" component={Comidas} />
+        <Route path="/explorar/comidas/ingredientes" component={ExplorarComidasIngredientes} />
+        <Route path="/explorar/bebidas/ingredientes" component={ExplorarBebidasIngredientes} />
+        <Route path="/explorar/comidas/area" component={ExplorarComidasArea} />
+        <Route path="/explorar/comidas" component={ExplorarComidas} />
+        <Route path="/explorar/bebidas" component={ExplorarBebidas} />
+        <Route path="/explorar" component={Explorar} />
+        <Route path="/perfil" component={Perfil} />
+        <Route path="/receitas-feitas" component={ReceitasFeitas} />
+        <Route path="/receitas-favoritas" component={ReceitasFavoritas} />
+      </Switch>
     </ProviderSearch>
   );
 }
