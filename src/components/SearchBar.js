1c0494cbb5e682a0a0fcbd8bd8189cf52e893068
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-26 19:25:36 -0300

    implementando o input e os radios buttons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
 // comecando os trabalhos
 
-const SearchBar = () => <div data-testid="search-input">
-  <p>SearchBar</p>
-</div>;
+const SearchBar = () =>
+  <div data-testid="search-input">
+    <input type="text" name="searchBar"/>
+      <div>
+        <input data-testid="ingredient-search-radio" key="Ingrediente" type="radio" name="filter" /><span>Ingrediente</span>
+        <input data-testid="name-search-radio" key="Nome" type="radio" name="filter" /><span>Nome</span>
+        <input data-testid="first-letter-search-radio" key="PrimeiraLetra" type="radio" name="filter" /><span>Primeira Letra</span>
+      </div>
+      <div>
+        <button data-testid="exec-search-btn">Buscar</button>
+      </div>
+  </div>;
 
 export default SearchBar;
