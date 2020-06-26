9eba08eb49e58db77839873c645d4207f1eb4a6a
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-26 19:30:45 -0300

    codeclimate

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,18 @@ import React from 'react';
 
 const SearchBar = () =>
   <div data-testid="search-input">
-    <input type="text" name="searchBar"/>
-      <div>
-        <input data-testid="ingredient-search-radio" key="Ingrediente" type="radio" name="filter" /><span>Ingrediente</span>
-        <input data-testid="name-search-radio" key="Nome" type="radio" name="filter" /><span>Nome</span>
-        <input data-testid="first-letter-search-radio" key="PrimeiraLetra" type="radio" name="filter" /><span>Primeira Letra</span>
-      </div>
-      <div>
-        <button data-testid="exec-search-btn">Buscar</button>
-      </div>
+    <input type="text" name="searchBar" />
+    <div>
+      <input data-testid="ingredient-search-radio" key="Ingrediente" type="radio" name="filter" />
+      <span>Ingrediente</span>
+      <input data-testid="name-search-radio" key="Nome" type="radio" name="filter" />
+      <span>Nome</span>
+      <input data-testid="first-letter-search-radio" key="PrimeiraLetra" type="radio" name="filter" />
+      <span>Primeira Letra</span>
+    </div>
+    <div>
+      <button data-testid="exec-search-btn">Buscar</button>
+    </div>
   </div>;
 
 export default SearchBar;
