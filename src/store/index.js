00147ef00ea792343f372762e3cdbcd0fc96b35c
tamagoshii <estelaap_martins@hotmail.com>
2020-06-29 18:19:57 -0300

    validando req Login Page

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(composeEnhancer(applyMiddleware(thunk, logger)));
-
-export default store;
