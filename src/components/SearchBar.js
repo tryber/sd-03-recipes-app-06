b59185a318383ad94dcf3d1f1f6be03c43b13a36
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-29 19:01:51 -0300

    passando em 9 requisitos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,65 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { getMealByLetter, getMealByIngredients, getMealByName } from '../services/MealDBApi';
+import { getDrinkByLetter, getDrinkByIngredients, getDrinkByName } from '../services/DrinkDBApi';
 
 const SearchBar = () => {
+  const location = useLocation();
   const [selected, setSelected] = useState('name');
   const [search, setSearch] = useState('');
 
-  const handleChange = () => {
+  const mealtSearch = () => {
     if (selected === 'name') {
       const result = getMealByName(search);
       console.log(result);
     } else if (selected === 'ingredient') {
       const result = getMealByIngredients(search);
       console.log(result);
-    } else if (selected === 'letter') {
+    } else if (selected === 'letter' && search.length === 1) {
       const result = getMealByLetter(search);
       console.log(result);
+    } else if (selected === 'letter' && search.length > 1) {
+      alert('Sua busca deve conter somente 1 (um) caracter');
+    } 
+  };
+
+  const drinkSearch = () => {
+    if (selected === 'name') {
+      const result = getDrinkByName(search);
+      console.log(result);
+    } else if (selected === 'ingredient') {
+      const result = getDrinkByIngredients(search);
+      console.log(result);
+    } else if (selected === 'letter' && search.length === 1) {
+      const result = getDrinkByLetter(search);
+      console.log(result);
+    } else if (selected === 'letter' && search.length > 1) {
+      alert('Sua busca deve conter somente 1 (um) caracter');
+    } 
+  };
+
+  const handleChange = () => {
+    if (location.pathname.match(/comidas/g)) {
+      mealtSearch();
+    } else if (location.pathname.match(/bebidas/g)) {
+      drinkSearch();
     }
   };
 
   return (
-    <div data-testid="search-input">
-      <input type="text" name="searchBar" onChange={(e) => setSearch(e.target.value)} />
+    <div>
+      <input type="text" data-testid="search-input" name="searchBar" onChange={(e) => setSearch(e.target.value)} />
       <div>
         <input
-          data-testid="ingredient-search-radio" key="Ingrediente" type="radio"
+          type="radio" data-testid="ingredient-search-radio" key="Ingrediente"
           name="filter" onClick={() => setSelected('ingredient')}
         /><span>Ingrediente</span>
         <input
-          data-testid="name-search-radio" key="Nome" type="radio"
+          type="radio" data-testid="name-search-radio" key="Nome"
           name="filter" onClick={() => setSelected('name')}
         /><span>Nome</span>
         <input
-          data-testid="first-letter-search-radio" key="PrimLetra" type="radio"
+          type="radio" data-testid="first-letter-search-radio" key="PrimLetra"
           name="filter" onClick={() => setSelected('letter')}
         /><span>Primeira Letra</span>
       </div>
