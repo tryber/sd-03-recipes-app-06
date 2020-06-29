c5a61722f6f5899b559143aa0ad8c94bd4a1d5bd
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-29 10:38:41 -0300

    Personalizando busca

--- src/services/MealDBApi.js
+++ src/services/MealDBApi.js
@@ -1,7 +1,19 @@
-const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
 
-const getMealByLetter = () => fetch(BASE_URL).then((response) => response
+const getMealByLetter = (letter) => fetch(`${BASE_URL}${letter}`).then((response) => response
   .json()
   .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
 
-export default getMealByLetter;
+const ING_URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+
+export const getMealByIngredients = (ingredient) =>
+  fetch(`${ING_URL}${ingredient}`).then((response) => response
+    .json()
+    .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
+
+const NAME_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+export const getMealByName = (name) =>
+  fetch(`${ING_URL}${name}`).then((response) => response
+    .json()
+    .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))));
