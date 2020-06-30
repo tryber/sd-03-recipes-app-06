5a9a3955654b2acb20136283338a22d12861596c
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-06-30 15:09:25 -0300

    lets see

--- src/context/getMeals.js
+++ src/context/getMeals.js
@@ -2,19 +2,16 @@ import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
 import useGetCategories from '../hooks/useGetCategories';
 import useFetchMeals from '../hooks/useFetchMeals';
-import useSelectCategory from '../hooks/useSelectCategory';
 
 const GetMealsContext = createContext();
 
 const Provider = ({ children }) => {
   const getCategories = useGetCategories();
   const getMeals = useFetchMeals();
-  const getSelectCategory = useSelectCategory();
 
   const context = {
     getMeals,
     getCategories,
-    getSelectCategory,
   };
 
   return (
