2550f54a49b21b9304d1360f4835a026807c1e4b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-30 16:29:51 -0300

    tranformando o handleCange em assincrono

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,44 +3,46 @@ import { useLocation } from 'react-router-dom';
 import { getMealByLetter, getMealByIngredients, getMealByName } from '../services/MealDBApi';
 import { getDrinkByLetter, getDrinkByIngredients, getDrinkByName } from '../services/DrinkDBApi';
 
+const mealtSearch = async (filter, arg) => {
+  if (filter === 'name') {
+    const result = await getMealByName(arg).then((response) => response.meals);
+    return result;
+  } else if (filter === 'ingredient') {
+    const result = await getMealByIngredients(arg).then((response) => response.meals);
+    return result;
+  } else if (filter === 'letter' && arg.length === 1) {
+    const result = await getMealByLetter(arg).then((response) => response.meals);
+    return result;
+  } else {
+    alert('Sua busca deve conter somente 1 (um) caracter');
+  }
+};
+
+const drinkSearch = async (filter, arg) => {
+  if (filter === 'name') {
+    const result = await getDrinkByName(arg).then((response) => response.drinks);
+    return result;
+  } else if (filter === 'ingredient') {
+    const result = await getDrinkByIngredients(arg).then((response) => response.drinks);
+    return result;
+  } else if (filter === 'letter' && arg.length === 1) {
+    const result = await getDrinkByLetter(arg).then((response) => response.drinks);
+    return result;
+  } else if (filter === 'letter') {
+    alert('Sua busca deve conter somente 1 (um) caracter');
+  }
+};
+
 const SearchBar = () => {
   const location = useLocation();
   const [selected, setSelected] = useState('name');
   const [search, setSearch] = useState('');
 
-  const mealtSearch = async (filter, arg) => { 
-    if (filter === 'name') {
-      const result = await getMealByName(arg).then((response) => response.meals);
-      console.log(result);
-    } else if (filter === 'ingredient') {
-      const result = await getMealByIngredients(arg).then((response) => response.meals);
-      console.log(result);
-    } else if (filter === 'letter' && arg.length === 1) {
-      const result = await getMealByLetter(arg).then((response) => response.meals);
-      console.log(result);
-    } else {
-      alert('Sua busca deve conter somente 1 (um) caracter');
-    }
-  };
-/*
-  const drinkSearch = async (filter, arg) => {
-    if (filter === 'name') {
-      const result = await getDrinkByName(arg).then((response) => response.drinks);
-      console.log(result);
-    } else if (filter === 'ingredient') {
-      const result = await getDrinkByIngredients(arg).then((response) => response.drinks);
-      console.log(result);
-    } else if (filter === 'letter' && arg.length === 1) {
-      const result = await getDrinkByLetter(arg).then((response) => response.drinks);
-      console.log(result);
-    } else if (filter === 'letter') {
-      alert('Sua busca deve conter somente 1 (um) caracter');
-    }
-  };
-*/
-  const handleChange = () => {
-    if (location.pathname.match(/comidas/g)) { mealtSearch(selected, search); }
-    // if (location.pathname.match(/bebidas/g)) { drinkSearch(selected, search); }
+  const handleChange = async () => {
+    let received;
+    if (location.pathname.match(/comidas/g)) { received = await mealtSearch(selected, search); }
+    if (location.pathname.match(/bebidas/g)) { received = await drinkSearch(selected, search); }
+    console.log(received);
   };
 
   return (
