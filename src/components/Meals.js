7dbee16c5ad67df3f9adef0d92e75b78b7fca706
tales <talestales@gmail.com>
2020-07-01 17:21:00 -0300

    cc4

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,51 +1,32 @@
 import React, { useContext } from 'react';
-import Context from '../context/Context';
+import { GetMealsContext } from '../context/getMeals';
 import MealCategories from './MealCategories';
 
 function Meals() {
   const {
-    meals,
-  } = useContext(Context);
-  <div>
-    if (loading) return <div>Loading...</div>;
-    return (
-      <div id="meals">
+    getMeals: {
+      meals,
+    },
+  } = useContext(GetMealsContext);
+return (
+    <div>
+    <MealCategories />
+    <ul>
       {
-        meals.map((meal) => {
-          const {
-            idMeal,
-            strMealThumb,
-            strMeal,
-            strCategory,
-          } = meal;
+        meals.slice(0, 12).map((meal) => {
+          const { idMeal, strMealThumb, strMeal, strCategory } = meal;
           return (
-            <li key={idMeal}>
-              <img src={strMealThumb} width="120px" height="150px" alt="Meal Thumb" />
-              {strMeal} - {strCategory}
-            </li>
+            <div className="card" key={idMeal}>
+              <li>
+                <img src={strMealThumb} width="120px" height="150px" alt="Meal Thumb" />
+                {strMeal} - {strCategory}
+              </li>
+            </div>
           );
         })
       }
-    </div>
-    <div>
-      <MealCategories />
-      <ul>
-        {
-          meals.slice(0, 12).map((meal) => {
-            const { idMeal, strMealThumb, strMeal, strCategory } = meal;
-            return (
-              <div className="card" key={idMeal}>
-                <li>
-                  <img src={strMealThumb} width="120px" height="150px" alt="Meal Thumb" />
-                  {strMeal} - {strCategory}
-                </li>
-              </div>
-            );
-          })
-        }
-      </ul>
-    </div>
-  </div>;
+    </ul>
+  </div>
+);
 }
-
 export default Meals;
