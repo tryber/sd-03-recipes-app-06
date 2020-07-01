b472aaa2fecdec60bb8971b3526ae1d7ea49237e
tales <talestales@gmail.com>
2020-07-01 17:22:51 -0300

    cc5

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -8,25 +8,25 @@ function Meals() {
       meals,
     },
   } = useContext(GetMealsContext);
-return (
+  return (
     <div>
-    <MealCategories />
-    <ul>
-      {
-        meals.slice(0, 12).map((meal) => {
-          const { idMeal, strMealThumb, strMeal, strCategory } = meal;
-          return (
-            <div className="card" key={idMeal}>
-              <li>
-                <img src={strMealThumb} width="120px" height="150px" alt="Meal Thumb" />
-                {strMeal} - {strCategory}
-              </li>
-            </div>
-          );
-        })
-      }
-    </ul>
-  </div>
-);
+      <MealCategories />
+      <ul>
+        {
+          meals.slice(0, 12).map((meal) => {
+            const { idMeal, strMealThumb, strMeal, strCategory } = meal;
+            return (
+              <div className="card" key={idMeal}>
+                <li>
+                  <img src={strMealThumb} width="120px" height="150px" alt="Meal Thumb" />
+                  {strMeal} - {strCategory}
+                </li>
+              </div>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
 }
 export default Meals;
