34f339385849fb39207221bc69c04b03a6a504ab
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-02 08:56:14 -0300

    codeclimate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 const Footer = () =>
-    <div className="footer-container" data-testid="footer">
-      <Link to="/bebidas"><img src={drinkIcon} alt="drink" data-testid="drinks-bottom-btn" /></Link>
-      <Link to="/explorar">
-        <img src={exploreIcon} alt="explore" data-testid="explore-bottom-btn" />
-      </Link>
-      <Link to="/comidas"><img src={mealIcon} alt="meal" data-testid="food-bottom-btn" /></Link>
-    </div>
+  <div className="footer-container" data-testid="footer">
+    <Link to="/bebidas"><img src={drinkIcon} alt="drink" data-testid="drinks-bottom-btn" /></Link>
+    <Link to="/explorar">
+      <img src={exploreIcon} alt="explore" data-testid="explore-bottom-btn" />
+    </Link>
+    <Link to="/comidas"><img src={mealIcon} alt="meal" data-testid="food-bottom-btn" /></Link>
+  </div>;
 
 export default Footer;
