62ac3c7f4f795c3b2b0c20e9bf5b23e95cbf9ab7
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-02 09:33:01 -0300

    Tela de Explorar Feita

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Explorar = () =>
   <div>
     <Header />
-    <p>Esta é a página de Explorar</p>
+    <Link to="/explorar/comidas">
+      <button data-testid="explore-food">Explorar Comidas</button>
+    </Link>
+    <Link to="/explorar/bebidas">
+      <button data-testid="explore-drinks">Explorar Bebidas</button>
+    </Link>
     <Footer />
   </div>;
 
