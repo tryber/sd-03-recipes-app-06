2c34ad9dbd3066806ef66b001ba6b1eb5c2b2bc7
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-07-07 17:52:00 -0300

    adding proptypes

--- src/pages/FilterDrinks.js
+++ src/pages/FilterDrinks.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { getDrinkByIngredients } from '../services/DrinkDBApi';
